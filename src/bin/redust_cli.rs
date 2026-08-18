1e5d426b27c20983279f52fecb4d30328c699806
Siddhesh Pardeshi <siddheshpardeshi2@gmail.com>
2026-08-18 21:33:49 +0530

    use tokio finish cli with support for get and set

--- src/bin/redust_cli.rs
+++ src/bin/redust_cli.rs
@@ -1,19 +1,48 @@
-use std::io::{Write, stdin, stdout};
+use std::{
+    env::args,
+    io::{Write, stdin, stdout},
+};
 
-fn main() {
+use tokio::io::AsyncBufReadExt;
+use tokio::io::AsyncWriteExt;
+
+#[tokio::main]
+async fn main() -> () {
+    let args: Vec<String> = args().collect();
+    if args.len() < 3 {
+        eprintln!("usage: $./redust_cli <host> <port>");
+        ()
+    }
+    let [_file_path, host, port] = args.try_into().expect("filed to exact host and port");
+    let conn = tokio::net::TcpStream::connect(format!("{host}:{port}"))
+        .await
+        .expect("Failed to connect to {host}:{port}");
+
+    let (reader, mut writer) = conn.into_split();
+    let mut reader = tokio::io::BufReader::new(reader);
+
     loop {
         print!("> ");
         stdout().flush().expect("Could not flush. LoL!:)");
-        let mut buf = String::new();
+        let mut command: String = String::new();
 
-        let handle = stdin().read_line(&mut buf);
-        match handle {
-            Ok(_read_bytes) => {
-                print!("you said: {buf}");
+        stdin().read_line(&mut command).unwrap();
+
+        if let Err(err) = writer.write_all(command.to_string().as_bytes()).await {
+            eprintln!("{err}");
+        }
+
+        let mut response = String::new();
+
+        match reader.read_line(&mut response).await {
+            Ok(0) => {
+                eprintln!("Server closed connection");
+                break;
             },
-            Err(err) => {
-                eprintln!("Failed to parse command: {err}");
-            }
+            Ok(_) => {
+                print!("{response}");
+            },
+            Err(err) => { eprintln!("{err}"); }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use std::collections::{HashMap};
-use std::io::{ Read, Write};
-use std::net::TcpStream;
+use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-use std::{net::TcpListener, };
-use std::{ thread};
 
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 struct Map {
     map_string_string: HashMap<Box<str>, Box<str>>,
@@ -30,75 +27,91 @@ fn parse_command(command: String) -> std::io::Result<Vec<String>> {
     return Ok(args);
 }
 
-fn handle_connection(mut tcp_stream: TcpStream, map: Arc<Mutex<Map>>) {
-    let mut buf: [u8; 1<<10] = [0; 1<<10];
-    let n = tcp_stream.read(&mut buf).expect("Failed to read");
-
-    println!("read {} bytes ...", n);
-
-    let command = String::from_utf8_lossy(&mut buf[..n]).into_owned();
-    let command_parsed = parse_command(command);
-    println!("commands_parsed = {:?}", command_parsed);
-    match command_parsed {
-        Ok(command_vec) => {
-            if command_vec[0] == "set" && command_vec.len() == 3 {
-                let mut map_locked = map.lock().expect("Failed to lock map");
-                let key = command_vec[1].clone();
-                let val = command_vec[2].clone();
-
-                let k = key.trim();
-                let v = val.trim();
-                map_locked.map_string_string.insert(Box::from(k), Box::from(v));
-
-                drop(map_locked);
-                let _ = write!(tcp_stream, "set {k} to {v}").map_err(|err| {
-                    eprintln!("Filed to write to tcpStream: {err}");
-                });
+async fn handle_connection(mut tcp_stream: tokio::net::TcpStream, map: Arc<Mutex<Map>>) {
+    loop {
+        let mut buf: [u8; 1 << 10] = [0; 1 << 10];
+        let n = match tcp_stream.read(&mut buf).await {
+            Ok(0) => {
+                eprintln!("Client closed the connection");
+                return;
+            },
+            Ok(n) => n,
+            Err(err) => {
+                eprintln!("{err}");
+                return;
             }
+        };
 
-            if command_vec[0] == "get" && command_vec.len() == 2 {
-                let map_locked = map.lock().expect("Failed to lock map");
-                let k = command_vec[1].clone();
-                let kk = k.trim();
+        println!("read {} bytes ...", n);
 
-                let v = map_locked.map_string_string.get(kk);
-                let mut value = String::from("");
-                match v {
-                    Some(val) => {
-                        value = val.to_string();
-                    },
-                    None => {}
+        let command = String::from_utf8_lossy(&mut buf[..n]).into_owned();
+        let command_parsed = parse_command(command);
+        println!("commands_parsed = {:?}", command_parsed);
+        match command_parsed {
+            Ok(command_vec) => {
+                if command_vec[0] == "set" && command_vec.len() == 3 {
+                    let key = command_vec[1].clone();
+                    let val = command_vec[2].clone();
+
+                    let k = key.trim();
+                    let v = val.trim();
+
+                    {
+                        let mut map_locked = map.lock().expect("Failed to lock map");
+
+                        map_locked
+                            .map_string_string
+                            .insert(Box::from(k), Box::from(v));
+                    } // MutexGuard dropped here
+
+                    let response = format!("set {k} to {v}");
+
+                    let _ = tcp_stream
+                        .write_all(format!("{response}\n").to_string().as_bytes())
+                        .await
+                        .map_err(|err| {
+                            eprintln!("Filed to write to tcpStream: {err}");
+                        });
                 }
-                let _ = write!(tcp_stream, "{value}").map_err(|err| {
-                    eprintln!("Filed to write to tcpStream: {err}");
-                });
+
+                if command_vec[0] == "get" && command_vec.len() == 2 {
+                    let k = command_vec[1].trim();
+
+                    let value = {
+                        let map_locked = map.lock().expect("failed to aquire lock on map");
+                        match map_locked.map_string_string.get(k) {
+                            Some(value) => value.to_string(),
+                            None => String::new(),
+                        }
+                    };
+                    let _ = tcp_stream
+                        .write_all(format!("{value}\n").to_string().as_bytes())
+                        .await
+                        .map_err(|err| {
+                            eprintln!("Filed to write to tcpStream: {err}");
+                        });
+                }
             }
-        }, 
-        Err(_err) => {}
+            Err(_err) => {}
+        }
     }
 }
 
-fn main() -> std::io::Result<()> {
-    let tcp = TcpListener::bind("127.0.0.1:8000");
-    let map = Arc::new(Mutex::new(Map {map_string_string: HashMap::new()}));
-    match tcp {
-        Ok(conn) => {
-            for stream in conn.incoming() {
-                match stream {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let tcp = tokio::net::TcpListener::bind("127.0.0.1:8000");
+    let map = Arc::new(Mutex::new(Map {
+        map_string_string: HashMap::new(),
+    }));
+    match tcp.await {
+        Ok(conn) => loop {
+            let (stream, _socket_addr) = conn.accept().await.unwrap();
 
-                    Ok(tcp_stream) => {
-                        let map_clone: Arc<Mutex<Map>> = Arc::clone(&map);
+            let map_clone: Arc<Mutex<Map>> = Arc::clone(&map);
 
-                        thread::spawn(move || {
-                            handle_connection(tcp_stream, map_clone);
-                        });
-                    },
-                    Err(err) => {
-                        println!("Something wrong with tcp connection {:?}", err);
-                    }
-                }
-            }
-
+            tokio::spawn(async move {
+                handle_connection(stream, map_clone).await;
+            });
         },
         Err(err) => {
             println!("Failed to lauch TCP Server: {:?}", err);
